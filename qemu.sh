34b0ecfd1221a5103e05053b8775e388fa6fd55d
Abhinav Kota <abhinav.kota06@gmail.com>
2025-07-09 21:27:40 -0400

    Added debugging option to QEMU testing script

--- qemu.sh
+++ qemu.sh
@@ -2,4 +2,10 @@
 set -e
 . ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom hippos.iso -s -S
+if [ "$#" -eq 1 ] && [ "$1" = "--debug" ]; then
+    echo "Debugging mode"
+    qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom hippos.iso -s -S
+else
+    echo "Normal mode"
+    qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom hippos.iso
+fi
